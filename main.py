41cb9a83e740ab13dd27b48c4dc6796c06c75e3c
Rajnish Kumar Gupta <44259600+geekyrajnish@users.noreply.github.com>
2023-02-14 01:21:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from functions import run
-import sqlite3
+# import sqlite3
 import validators
 from validators import ValidationFailure
 import urllib.request
@@ -35,14 +35,14 @@ def website_is_up(url):
 def main(url):
     response = {}
 
-    conn = sqlite3.connect("database.db")
-    cur = conn.cursor()
-    cur.execute("select count from phishcount where key=1")
-    curr_count = cur.fetchall()[0][0]
-    cur.execute("select count from phishcount where key=1")
-    cur.execute(f"update phishcount set count = {curr_count + 1} where key = 1")
-    conn.commit()
-    conn.close()
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+#     cur.execute("select count from phishcount where key=1")
+#     curr_count = cur.fetchall()[0][0]
+#     cur.execute("select count from phishcount where key=1")
+#     cur.execute(f"update phishcount set count = {curr_count + 1} where key = 1")
+#     conn.commit()
+#     conn.close()
 
     if(not is_string_an_url(url)):
         response["status-code"] = 11
